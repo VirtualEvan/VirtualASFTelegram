de417d53afa76dc09046a659456bd517b69757a0
VirtualEvan <virtualevan@gmail.com>
2017-09-20 12:23:55 +0200

    adding handlers

--- asfbot.py
+++ asfbot.py
@@ -10,11 +10,14 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="This is a starting message")
 
 
-def ownerchat(bot, update):
+def owner_chat_id(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.chat_id)
 
 
 start_handler = CommandHandler('start', start)
+owner_handler = CommandHandler('ownerchat', start)
+
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(owner_handler)
 
 updater.start_polling()
