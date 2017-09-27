5b9305a1ad08c5530738a1e3f177148a127ade07
VirtualEvan <virtualevan@gmail.com>
2017-09-27 11:28:26 +0200

    Inline Buttons for bot selection

--- asfbot.py
+++ asfbot.py
@@ -1,10 +1,17 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 import requests
 import config
 
 updater = Updater(token=config.get_token())
 
 dispatcher = updater.dispatcher
+
+
+def build_menu(buttons, n_cols,):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    return menu[0]
 
 
 def start(bot, update):
@@ -15,6 +22,7 @@ def owner_chat_id(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.chat_id)
 
 
+# STM
 def redeem(bot, update, args):
     params = (
         ('command', 'redeem 1-virtualevan ' + ' '.join(args)),
@@ -24,17 +32,37 @@ def redeem(bot, update, args):
     bot.send_message(chat_id=update.message.chat_id, text=response.text)
 
 
+def test(bot, update, args=None):
+    show_menu(bot, update, args)
+
+
 def addlicense(bot, update, args):
-    params = (
-        ('command', 'addlicense ASF ' + ' '.join(args)),
-    )
+    show_menu(bot, update, args)
+    # params = (
+    #     ('command', 'addlicense ASF ' + ' '.join(args)),
+    # )
+    #
+    # response = requests.get('http://127.0.0.1:1242/IPC', params=params)
+    # bot.send_message(chat_id=update.message.chat_id, text=response.text)
 
-    response = requests.get('http://127.0.0.1:1242/IPC', params=params)
+
+def show_menu(bot, update, args=None):
+    button_list = [
+        [InlineKeyboardButton("VirtualEvan", callback_data=update.message.text[1:] + ' ' + '1-virtualevan' + ' '.join(args)),
+         InlineKeyboardButton("FidelKarthus", callback_data=update.message.text[1:] + ' ' + '2-fidelkarthus' + ' '.join(args))],
+        [InlineKeyboardButton("Nikolai", callback_data=update.message.text[1:] + ' ' + '3-nikolai' + ' '.join(args)),
+         InlineKeyboardButton("Ho Chi Minh", callback_data=update.message.text[1:] + ' ' + '4-hochiminh' + ' '.join(args))],
+        [InlineKeyboardButton("All of them", callback_data=update.message.text[1:] + ' ' + 'ASF' + ' '.join(args))]
+    ]
+    reply_markup = InlineKeyboardMarkup(build_menu(button_list, n_cols=3))
+    bot.send_message(chat_id=update.message.chat_id, text="Which bot?", reply_markup=reply_markup)
+
+
+def button(bot, update):
+    bot.deleteMessage(chat_id=update.callback_query.message.chat_id, message_id=update.callback_query.message.message_id)
+
+    response = requests.get('http://127.0.0.1:1242/IPC?command=' + update.callback_query.data)
     bot.send_message(chat_id=update.message.chat_id, text=response.text)
-
-
-def stop(bot, update):
-    pass
 
 
 def block(bot, update):
@@ -44,13 +72,22 @@ def block(bot, update):
 block_handler = MessageHandler(~ Filters.chat(156195413), block)
 dispatcher.add_handler(block_handler)
 
-# STM commands
+# Query handler
+query_handler = CallbackQueryHandler(button)
+dispatcher.add_handler(query_handler)
+
+# STM handlers
 redeem_handler = CommandHandler('redeem', redeem, pass_args=True)
 addlicense_handler = CommandHandler('addlicense', addlicense, pass_args=True)
 
+# Bot handlers
 start_handler = CommandHandler('start', start)
 owner_handler = CommandHandler('ownerchat', owner_chat_id)
-stop_handler = CommandHandler('stop', stop)
+stop_handler = CommandHandler('stop', show_menu)
+
+test_handler = CommandHandler('test', test, pass_args=True)
+dispatcher.add_handler(test_handler)
+
 
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(owner_handler)
